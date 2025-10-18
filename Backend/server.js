00a8fa0b2ea1e3d62311b7076f8f0ca42faf8642
raymond <raymondmapayo@gmail.com>
2025-10-19 02:41:56 +0800

    Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
 const multer = require("multer");
